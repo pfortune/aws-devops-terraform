f741775326d2ad6fc251b2e10be6b523bac458c1
Peter Fortune <pfortune@gmail.com>
2024-04-15 13:38:49 +0100

    Added state management for prod

--- prod/versions.tf
+++ prod/versions.tf
@@ -5,4 +5,13 @@ terraform {
       version = "5.44.0"
     }
   }
+
+    backend "s3" {
+    bucket = "terraform-state-devops-2024"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-state-lock-devops-2024"
+    encrypt        = true
+  }
 }
